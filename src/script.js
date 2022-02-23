f9f83290afe4e7bbed214e46044c163a79fb0ebe
bryan <bryanf777@gmail.com>
2022-02-23 18:14:35 -0500

    first commit

--- src/script.js
+++ src/script.js
@@ -1,3 +1,24 @@
-"use strict";
 
-// TODO - write your code here.
+let randomDamage = () => {
+        Math.floor(Math.random(1) * 10);
+} 
+
+
+const chooseoption = (opt1, opt2) => {
+    const randNum = Math.floor(math.random() * 2);
+    randNum === 0 ? opt1 : opt2;
+} 
+
+const attackplayer = function(health){
+    return health - randomDamage;
+}
+
+const loghealth = (player, health) {
+    console.log( ${player1} health: ${health})
+}
+const logDeath = (winner, loser) => {
+    console.log(`${winner} defeated ${loser}`);
+} 
+const isDead = (health) => {
+    return health <= 0;
+}
